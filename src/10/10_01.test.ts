eb8318b4c252444ed4ad9a59c79b017e0fa10718
yuliaslatvitskaya <yuli.slat@yandex.ru>
2022-10-21 20:30:12 +0200

    new function moveUser,moveUserToOtherHouse 10_01

--- src/10/10_01.test.ts
+++ src/10/10_01.test.ts
@@ -1,23 +1,31 @@
-import {moveUser, UserType, UserWithLapTopType} from "./10_01";
+import {
+    addNewBooksToUser,
+    moveUser,
+    moveUserToOtherHouse,
+    upgradeUserLaptop,
+    UserType,
+    UserWithBooksType,
+    UserWithLapTopType
+} from "./10_01";
 
-function makeHairstyle(u:UserType,power:number){
-    const copy={
+function makeHairstyle(u: UserType, power: number) {
+    const copy = {
         ...u
     }
-    copy.hair=u.hair / power
-    u.hair=u.hair / power
+    copy.hair = u.hair / power
+    u.hair = u.hair / power
     return copy
 }
 
-test ('reference type test' ,()=>{
-    let user :UserType={
-        name:'Yulia',
-        hair:24,
-        address:{
-            city:'Alta'
+test('reference type test', () => {
+    let user: UserType = {
+        name: 'Yulia',
+        hair: 24,
+        address: {
+            city: 'Alta'
         }
     }
-   const  awesomeUser=makeHairstyle(user,2)
+    const awesomeUser = makeHairstyle(user, 2)
 
     expect(user.hair).toBe(12)
     expect(awesomeUser.address).toBe(user.address)
@@ -26,17 +34,16 @@ test ('reference type test' ,()=>{
 })
 
 
-
-test ('change address' ,()=>{
-    let user :UserWithLapTopType={
-        name:'Yulia',
-        hair:24,
-        address:{
-            city:'Alta',
-            house:12
+test('change address', () => {
+    let user: UserWithLapTopType = {
+        name: 'Yulia',
+        hair: 24,
+        address: {
+            city: 'Alta',
+            house: 12
         },
-        laptop:{
-            title:'ZenBook'
+        laptop: {
+            title: 'ZenBook'
         }
 
     }
@@ -48,5 +55,81 @@ test ('change address' ,()=>{
     expect(user.laptop).toBe(movedUser.laptop)
     expect(movedUser.address.city).toBe('Kiev')
 
+
+})
+
+
+test('upgrade laptop to macbook', () => {
+    let user: UserWithLapTopType = {
+        name: 'Yulia',
+        hair: 24,
+        address: {
+            city: 'Alta',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+
+    }
+    const macbook = upgradeUserLaptop(user, 'macbook');
+
+
+    expect(user).not.toBe(macbook)
+    expect(user.address).toBe(macbook.address)
+    expect(user.laptop).not.toBe(macbook.laptop)
+    expect(macbook.laptop.title).toBe('macbook')
+    expect(user.laptop.title).toBe('ZenBook')
+
+
+})
+
+test('upgrade laptop to macbook', () => {
+    let user: UserWithLapTopType & UserWithBooksType = {
+        name: 'Yulia',
+        hair: 24,
+        address: {
+            city: 'Alta',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: ['css', 'html', 'js', 'react']
+
+    }
+    const macbook = moveUserToOtherHouse(user, 99);
+
+
+    expect(user).not.toBe(macbook)
+    expect(user.books).toBe(macbook.books)
+    expect(user.laptop).toBe(macbook.laptop)
+    expect(user.address).not.toBe(macbook.address)
+    expect(user.address.house).toBe(99)
+
+})
+
+test('add new books to user', () => {
+    let user: UserWithLapTopType & UserWithBooksType = {
+        name: 'Yulia',
+        hair: 24,
+        address: {
+            city: 'Alta',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        },
+        books: ['css', 'html', 'js', 'react']
+
+    }
+    const macbook = addNewBooksToUser(user, 'ts');
+
+
+    expect(user).not.toBe(macbook)
+    expect(user.laptop).toBe(macbook.laptop)
+    expect(user.address).toBe(macbook.address)
+    expect(user.books).not.toBe(macbook.books)
+    expect(macbook.books[4]).toBe('ts')
 
 })
--- src/10/10_01.ts
+++ src/10/10_01.ts
@@ -1,31 +1,69 @@
-export type UserType={
-    name:string
-    hair:number
-    address:{city:string,house?:number}
+export type UserType = {
+    name: string
+    hair: number
+    address: { city: string, house?: number }
 }
 
-export type LapTopType={
-    title:string
+export type LapTopType = {
+    title: string
 }
 
-export type UserWithLapTopType= UserType&{
-    laptop:LapTopType
+export type UserWithLapTopType = UserType & {
+    laptop: LapTopType
 }
-export function makeHairstyle(u:UserType,power:number){
-    const copy={
+
+export type UserWithBooksType = UserType & {
+    books: Array<string>
+}
+
+export function makeHairstyle(u: UserType, power: number) {
+    const copy = {
         ...u
     }
-    copy.hair=u.hair / power
-    u.hair=u.hair / power
+    copy.hair = u.hair / power
+    u.hair = u.hair / power
     return copy
 }
-export function moveUser(l:UserWithLapTopType,city:string){
-    const copy={
+
+export function moveUser(l: UserWithLapTopType, city: string) {
+    return {
         ...l,
+        address: {
+            ...l.address,
+            city: city
+        }
     }
-    copy.address ={
-        ...copy.address,
-        city:city
+
+}
+
+export function moveUserToOtherHouse(u: UserWithLapTopType & UserWithBooksType, house: number) {
+    return {
+        ...u,
+        address: {
+            ...u.address,
+            house: house
+        }
     }
-   return copy
+
+}
+
+export function upgradeUserLaptop(u: UserWithLapTopType, laptop: string) {
+    return {
+        ...u,
+        laptop: {
+            ...u.laptop,
+            title: laptop
+        }
+    }
+
+}
+
+
+export function addNewBooksToUser(u: UserWithLapTopType & UserWithBooksType, newBooks: string) {
+    const copy = {
+        ...u,
+        books: [...u.books]
+    }
+    copy.books.push(newBooks)
+    return copy;
 }
